f539b0605a97324c3deb4a6b442ed76a855b378f
Alexander Nygaard <54611459+Alex-Nygaard@users.noreply.github.com>
2020-10-12 08:30:53 +0900

    Add files via upload

---
+++ project_week/static/scripting.js
@@ -0,0 +1,13 @@
+
+var timer
+
+var myVar = serInterval(myTimer, 1000)
+
+function testFunc() {
+    var startTime = new Date();
+    startTime = startTime.toLocaleTimeString();
+
+    document.getElementById("arr").style.top = "300px"
+    console.log("Running")
+
+}
